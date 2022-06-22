8ca6a542d485284288a9cd5562e50a5167a4dd23
Erick Avila <yahir7083@gmail.com>
2022-06-22 17:29:40 -0500

    1.0 stable version

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+import os
 from pytube import *
+from moviepy.editor import *
+
+
+def convert_to_mp3(folder):
+    """ Convert videos to mp3 """
+    downloads_folder = os.path.join(folder, "downloads")
+    for file in os.listdir(downloads_folder):
+        if file.endswith(".mp4"):
+            mp4_file_path = os.path.join(downloads_folder, file)
+            mp3_file_path = os.path.join(downloads_folder, file[:-4] + ".mp3")
+
+            video = VideoFileClip(mp4_file_path)
+            video.audio.write_audiofile(mp3_file_path)
+
+            print(f"[+] {mp4_file_path} converted to {mp3_file_path}")
+
+            os.remove(mp4_file_path)
+            print(f"[+] {mp4_file_path} removed")
 
 
 def main():
     """ Search the video by name and take the first_result, then create the url_video """
-    yt_search = Search("MBDTF - Kanye West")
+    yt_search = Search("Everybody Mac Miller")
     print("[+] Searching...")
 
     first_result = str(yt_search.results[0])
@@ -15,10 +34,12 @@ def main():
     print(f"[+] {yt_video.title} found!")
 
     # Dowload the video
-    seted_video = yt_video.streams.get_by_itag(139)
-    seted_video.download()
+    # print(yt_video.streams.filter(file_extension='mp4'))
+    seted_video = yt_video.streams.get_by_itag(18)
+    seted_video.download("/home/erick/workspace/youtube/downloads")
     print(f"[+] {yt_video.title} Downloaded!")
 
 
 if __name__ == "__main__":
     main()
+    convert_to_mp3("/home/erick/workspace/youtube")

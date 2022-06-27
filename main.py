630c6176dc97554cfe92088e0c16185344b8da25
Erick Avila <yahir7083@gmail.com>
2022-06-27 11:05:27 -0500

    Add error management

--- main.py
+++ main.py
@@ -27,24 +27,32 @@ def main():
     """ Search the video by name and take the first_result, then create the url_video """
     song = input("Enter the song name: ")
 
-    yt_search = Search(song)
-    print("[+] Searching...")
+    if song:
+        try:
+            yt_search = Search(song)
+            print("[+] Searching...")
 
-    first_result = str(yt_search.results[0])
-    id_video = first_result[41:52]
+            first_result = str(yt_search.results[0])
+            id_video = first_result[41:52]
 
-    url_video = f"https://youtu.be/{id_video}"
+            url_video = f"https://youtu.be/{id_video}"
 
-    yt_video = YouTube(url_video)
-    print(f"[+] {yt_video.title} found!")
+            yt_video = YouTube(url_video)
+            print(f"[+] {yt_video.title} found!")
 
-    # Dowload the video
-    # print(yt_video.streams.filter(file_extension='mp4'))
-    seted_video = yt_video.streams.get_by_itag(18)
-    seted_video.download("/home/erick/workspace/youtube/downloads")
-    print(f"[+] {yt_video.title} Downloaded!")
+            # Dowload the video
+            # print(yt_video.streams.filter(file_extension='mp4'))
+            seted_video = yt_video.streams.get_by_itag(18)
+            seted_video.download("/home/erick/workspace/youtube/downloads")
+            print(f"[+] {yt_video.title} Downloaded!")
+
+        except Exception as e:
+            print(f"[-] Has been an error: {e}")
+
+    else:
+        print("[-] You must enter a song name")
 
 
 if __name__ == "__main__":
     main()
-    convert_to_mp3("/home/erick/workspace/youtube")
+    # convert_to_mp3("/home/erick/workspace/youtube")
